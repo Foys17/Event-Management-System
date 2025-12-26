d865b127f48af226b5bbb8f25545cce185c53930
Foys17 <mhfoysal17@gmail.com>
2025-12-26 19:29:08 +0600

    Updated database settings for rezwan

--- Event_Management_System/settings.py
+++ Event_Management_System/settings.py
@@ -79,7 +79,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'event_management_db',      
         'USER': 'root',                     
-        'PASSWORD': 'password',  
+        'PASSWORD': '',  
         'HOST': 'localhost',                
         'PORT': '3306',                    
     }
